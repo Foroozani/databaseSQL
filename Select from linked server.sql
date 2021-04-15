05cd397566c24eac931321bd879f723a5e4f2685
Najmeh Foroozani <46888580+Foroozani@users.noreply.github.com>
2021-04-15 21:24:06 +0200

    Update Select from linked server.sql

--- Select from linked server.sql
+++ Select from linked server.sql
@@ -1,7 +1,2 @@
-﻿
-
 Select * from [Z_SHOBBAR].[FaraDars].[dbo].[tblLesson]
 
-insert into  [Z_SHOBBAR].[FaraDars].[dbo].[tblLesson]
-(code,title,field,unitnum)
-values (4910,N'هوش مصنوعی' , N'مهندسی نرم افزار',3 )
